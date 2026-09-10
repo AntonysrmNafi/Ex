98b049201b19f5aea236f61a141ec8818590dac4
Antonysrm Nafi <antonysrmnafi@gmail.com>
2026-09-10 06:27:30 +0600

    Add ExperimentalLayoutApi import to AddAccountCard.kt

--- app/src/main/java/com/blockveil/expense/tracker/ui/more/AddAccountCard.kt
+++ app/src/main/java/com/blockveil/expense/tracker/ui/more/AddAccountCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -48,6 +49,7 @@ import com.blockveil.expense.tracker.ui.theme.BrandPrimary
  * (name, loan amount, settlement amount, deposit-to account). Matches the add-account Card
  * in MoreScreen and handleAddAccount exactly.
  */
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun AddAccountCard(
     savingsAccounts: List<AccountEntity>,
