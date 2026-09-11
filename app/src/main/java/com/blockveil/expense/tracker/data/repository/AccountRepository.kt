ca73631ea4f50788bd64252ced1bd88a1df91f8d
Antonysrm Nafi <omidur74@gmail.com>
2026-09-11 08:39:23 +0600

    Add methods to observe visible accounts and set hidden state
    Added functions to observe visible accounts and set account visibility.

--- app/src/main/java/com/blockveil/expense/tracker/data/repository/AccountRepository.kt
+++ app/src/main/java/com/blockveil/expense/tracker/data/repository/AccountRepository.kt
@@ -10,6 +10,9 @@ class AccountRepository(db: AppDatabase) {
 
     fun observeAll(): Flow<List<AccountEntity>> = dao.observeAll()
 
+    /** Excludes hidden accounts, for anywhere an account can be picked for a new transaction/transfer or listed on Home. */
+    fun observeVisible(): Flow<List<AccountEntity>> = dao.observeVisible()
+
     suspend fun getById(id: Long): AccountEntity? = dao.getById(id)
 
     suspend fun insert(account: AccountEntity): Long = dao.insert(account)
@@ -17,6 +20,8 @@ class AccountRepository(db: AppDatabase) {
     suspend fun update(account: AccountEntity) = dao.update(account)
 
     suspend fun delete(account: AccountEntity) = dao.delete(account)
+
+    suspend fun setHidden(account: AccountEntity, hidden: Boolean) = dao.update(account.copy(isHidden = hidden))
 
     suspend fun deleteAll() = dao.deleteAll()
 }
