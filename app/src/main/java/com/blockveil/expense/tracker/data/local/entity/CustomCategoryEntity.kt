ccc8ed199968467c2b899d42fb1a84de2b4384d9
Antonysrm Nafi <205429236+AntonysrmNafi@users.noreply.github.com>
2026-09-14 18:00:55 +0600

    Add icon property to CustomCategoryEntity

--- app/src/main/java/com/blockveil/expense/tracker/data/local/entity/CustomCategoryEntity.kt
+++ app/src/main/java/com/blockveil/expense/tracker/data/local/entity/CustomCategoryEntity.kt
@@ -21,4 +21,6 @@ data class CustomCategoryEntity(
     /** Hidden categories drop out of the picker for new transactions but keep showing on
      *  past transactions that already used them, same as a fixed category being hidden. */
     val isHidden: Boolean = false,
+    /** Key into CUSTOM_CATEGORY_ICON_CHOICES (see CategoryIcons.kt); "Sell" if unset. */
+    val icon: String = "Sell",
 )
